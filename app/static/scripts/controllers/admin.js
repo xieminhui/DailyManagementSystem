ef41164e9d3084d662b1c6789dbf62c041903b15
xieminhui <xmh19940228@163.com>
2017-08-25 17:47:45 +0800

    DailyManagementSystem
    DailyManagementSystem code

---
+++ app/static/scripts/controllers/admin.js
@@ -0,0 +1,507 @@
+'use strict';
+
+//为adminApp模块（后台管理）定义控制器
+adminApp.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
+
+}]);
+
+//后台管理模块左侧导航栏控制器
+adminApp.controller('navCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
+        /**
+         * 需求：默认为显示导航第一个功能，点击导航切换右侧页面，且高亮当前导航项
+         */
+
+        //设定功能名称
+        var navGroups = [{
+            navName: '添加类别', //显示的名称
+            urlName: 'addSpendType' //对应URL的名称
+        }, {
+            navName: '管理全部类别',
+            urlName: 'seeAllType'
+        }, {
+            navName: '添加消费支出',
+            urlName: 'addSpending'
+        }, {
+            navName: '管理全部消费',
+            urlName: 'seeAllSpend'
+        }, {
+            navName: '添加管理员',
+            urlName: 'addAdmin'
+        }, {
+            navName: '更改密码',
+            urlName: 'manageAccount'
+        }];
+        $scope.navGroups = navGroups;
+    }
+]);
+
+//添加类别
+adminApp.controller('addSpendType', ['$scope', '$http',
+    function($scope, $http) {
+        //检验通过
+        $scope.addType = function(valid, event) {
+            $scope.formData = {
+                typeName: $scope.typeName
+            };
+            if (valid) {
+                //发送数据到后台
+                $http({
+                    method: 'POST',
+                    url: '/addSpendType',
+                    headers: {
+                        //表单的报头格式
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    data: $.param($scope.formData) //发送user数据到后台，这里用到了jQ
+                }).then(function successCallback(response) {
+                    if (response.status === 200) {
+                        $scope.typeName = "";
+                        alert(response.data.success);
+                    }
+                }, function errorCallback(response) {
+                    alert("添加类别失败");
+                });
+            }
+        }
+    }
+]);
+
+//查看全部类别
+adminApp.controller('seeAllType', ['$scope', '$http',
+    function($scope, $http) {
+        //异步获取页面数据
+        $scope.getPagedDataAsync = function(pageSize, page, searchText) {
+            setTimeout(function() {
+                var data;
+                if (searchText) {
+                    var ft = searchText.toLowerCase();
+                    //获取模拟的书籍json文件
+                    $http.get('/seeAllType')
+                        .success(function(largeLoad) {
+                            data = largeLoad.filter(function(item) {
+                                return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                            });
+                            $scope.setPagingData(data, page, pageSize);
+                        });
+                } else {
+                    $http({
+                        method: 'GET',
+                        url: '/seeAllType'
+                    }).then(function successCallback(response) {
+                        if (response.status === 200) {
+                            for(var i=0;i<response.data.length;i++){
+                                response.data[i].modify = 1;
+                                response.data[i].dele = 1;
+                            }
+                            $scope.spendTypes = response.data;
+
+                         //    $scope.setPagingData(response.data, page, pageSize);
+                        }
+                    }, function errorCallback(response) {
+                        alert("获取类别数据失败");
+                    });
+                }
+            }, 100);
+        };
+
+        $scope.getPagedDataAsync();
+
+        $scope.gridOptions = {
+            data: 'spendTypes',
+           // rowTemplate: '<div style="height: 100%"><div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell ">' +
+           //     '<div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }"> </div>' +
+           //      '<div ng-cell></div>' +
+           //     '</div></div>',
+            showGridFooter: true,
+            enableSorting: true,
+            columnDefs: [{
+                name: 'Sort_id',
+                displayName: 'id',
+                width: '10%',
+                // width: 10%,
+                pinnable: true,
+                sortable: true,
+                enableCellEdit: false
+            }, {
+                name: 'Sort_name',
+                displayName: '类别名',
+                enableCellEdit: true,
+                // width: 220
+                width: '30%'
+            }, {
+                name: 'modify',
+                displayName: '修改',
+                enableCellEdit: false,
+                sortable: false,
+                pinnable: false,
+                width: '30%',
+                // width: 120,
+                cellTemplate: '<div><a class="btn btn-xs btn-success feng-btn-modify" ng-click="grid.appScope.updateType(row)"  data="{{grid.getCellValue(row,col)}">确认修改</a></div>'
+            }, {
+                name: 'dele',
+                displayName: '删除',
+                enableCellEdit: false,
+                sortable: false,
+                pinnable: false,
+                // width: 120,
+                width: '30%',
+                cellTemplate: '<div><a class="btn btn-xs btn-danger feng-btn-delete" ng-click="grid.appScope.deleteType(row)" data="{{grid.getCellValue(row,col)}">删除</a></div>'
+                    //ng-click时间触发的时候传入row
+            }],
+            paginationPageSizes :[5, 10, 20],
+            paginationPageSize: 10
+        };
+
+        $scope.updateType = function(row) {
+            //包装数据传递到后台
+            var obj = {
+                id: row.entity.Sort_id,
+                typeName: row.entity.Sort_name
+            };
+            // console.log($.param(obj))
+            $http.put('/seeAllType/' + obj.id, obj).success(function(data, status) {
+                if (status === 200) {
+                    alert(data.success);
+                }
+            });
+        };
+
+        $scope.deleteType = function(row) {
+            $http.delete('/seeAllType/' + row.entity.Sort_id).success(function(data, status) {
+                if (status === 200) {
+                    $scope.getPagedDataAsync();
+                    alert(data.success);
+                }
+            });
+        };
+    }
+]);
+
+//添加消费支出
+adminApp.controller('addSpending', ['$scope', '$http','$filter',
+    function($scope, $http,$filter) {
+        //对类别进行加载
+        $http.get('/seeAllType')
+            .success(function(largeLoad) {
+                $scope.types = largeLoad;
+            });
+
+        //提交的时候
+        $scope.addBook = function(valid, event) {
+            $scope.formData = {
+                //bookName: $scope.bookName,
+                purchaser: $scope.purchaser,
+                typeId: $scope.typeId, //类别
+                price: $scope.price,
+                purchaserPlace: $scope.purchaserPlace,
+                purchaserDate: $filter('date')($scope.purchaserDate, 'yyyy-MM-dd'),
+               // sum: $scope.sum,
+                currentNum: $scope.currentNum,
+                brief: $scope.brief
+                    //缺少imageName,buyDate
+            };
+
+            //检验通过
+            if (valid) {
+                //发送数据到后台
+                $http({
+                    method: 'POST',
+                    url: '/addSpending',
+                    headers: {
+                        //表单的报头格式
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    data: $.param($scope.formData) //发送user数据到后台，这里用到了jQ
+                }).then(function successCallback(response) {
+                    if (response.status === 200) {
+                        //添加成功，清空输入项
+                        for (var p in $scope.formData) {
+                            $scope[p] = '';
+                        }
+                        alert(response.data.success);
+                    }
+                }, function errorCallback(response) {
+                    alert("添加类别失败");
+                });
+            }
+        }
+    }
+]);
+
+//查看全部消费
+adminApp.controller('seeAllSpend', ['$scope', '$http','$filter',
+    function($scope, $http,$filter) {
+        $scope.filterOptions = {
+            filterText: "",
+            useExternalFilter: true
+        };
+        $scope.totalServerItems = 0;
+        $scope.pagingOptions = {
+            pageSizes: [5, 10, 50],
+            pageSize: 10,
+            currentPage: 1
+        };
+
+        //设置页面数据
+        $scope.setPagingData = function(data, page, pageSize) {
+            for(var i=0;i<data.length;i++){
+                data[i].purchaserDate = new Date(data[i].purchaserDate).toLocaleDateString().replace(/\//g, '-');
+            }
+
+            var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+            $scope.spend = pagedData;
+            $scope.totalServerItems = data.length;
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        };
+
+        //异步获取页面数据
+        $scope.getPagedDataAsync = function(pageSize, page, searchText) {
+            setTimeout(function() {
+                var data;
+                if (searchText) {
+                    var ft = searchText.toLowerCase();
+                    //获取模拟的书籍json文件
+                    $http.get('/seeAllSpend')
+                        .success(function(largeLoad) {
+                            data = largeLoad.filter(function(item) {
+                                return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                            });
+                            $scope.setPagingData(data, page, pageSize);
+                        });
+                } else {
+                    $http({
+                        method: 'GET',
+                        url: '/seeAllSpend'
+                    }).then(function successCallback(response) {
+                        console.log(response);
+                        if (response.status === 200) {
+                            for(var i=0;i<response.data.length;i++){
+                                response.data[i].purchaserDate = new Date(response.data[i].purchaserDate);
+                            }
+                            $scope.spend = response.data;
+                            // $scope.setPagingData(response.data, page, pageSize);
+                        }
+                    }, function errorCallback(response) {
+                        alert("获取数据失败");
+                    });
+                }
+            }, 100);
+        };
+
+        $scope.getPagedDataAsync();
+
+        // //监听排序等
+        // $scope.$watch('pagingOptions', function(newVal, oldVal) {
+        //     if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+        //         $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        //     }
+        // }, true);
+        // $scope.$watch('filterOptions', function(newVal, oldVal) {
+        //     if (newVal !== oldVal) {
+        //         $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        //     }
+        // }, true);
+
+        $scope.gridOptions = {
+            data: 'spend',
+            // rowTemplate: '<div style="height: 100%"><div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell ">' +
+            //     '<div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }"> </div>' +
+            //     '<div ng-cell></div>' +
+            //     '</div></div>',
+            // multiSelect: false,
+            // enableCellSelection: true,
+            // enableRowSelection: false,
+            // enableCellEdit: true, //双击修改，单击选中
+            // enablePinning: true, //列固定
+            // enableColumnResize: true, //可以更改列宽度
+            columnDefs: [{
+                field: 'id',
+                displayName: 'id',
+                width: '5%',
+                pinnable: true,
+                sortable: true,
+                enableCellEdit: false
+            }, {
+                field: 'purchaser',
+                displayName: '购买者',
+                pinnable: true,
+                sortable: true,
+                enableCellEdit: true,
+                width: '10%'
+            }, {
+                field: 'Sort_name',
+                displayName: '类别',
+                pinnable: true,
+                sortable: true,
+                enableCellEdit: false,
+                width: '10%'
+            }, {
+                field: 'Sort_id',
+                displayName: '类别id',
+                visible: false //隐藏列，方便后面修改数据
+            }, {
+                field: 'Price',
+                displayName: '价格',
+                enableCellEdit: true,
+                width: '5%'
+            }, {
+                field: 'purchaserPlace',
+                displayName: '购买地点',
+                enableCellEdit: true,
+                width: '10%'
+            }, {
+                field: 'purchaserDate',
+                displayName: '购买日期',
+                type: 'date',
+                enableCellEdit: true,
+                cellFilter: 'date:"yyyy-MM-dd"', //过滤器，格式化日期
+                width: '15%'
+            }, {
+                field: 'Current_num',
+                displayName: '购买数量',
+                enableCellEdit: true,
+                width: '5%'
+            }, {
+                field: 'Brief',
+                displayName: '简介',
+                enableCellEdit: true,
+                width: '20%'
+            }, {
+                field: 'id',
+                displayName: '修改',
+                enableCellEdit: false,
+                sortable: false,
+                pinnable: false,
+                width: '10%',
+                // width: 120,
+                cellTemplate: '<div><a class="btn btn-xs btn-success feng-btn-modify" ng-click="grid.appScope.updateSpend(row)"  data="{{grid.getCellValue(row,col)}}">确认修改</a></div>'
+            }, {
+                field: 'id',
+                displayName: '删除',
+                enableCellEdit: false,
+                sortable: false,
+                pinnable: false,
+                // width: 120,
+                width: '10%',
+                cellTemplate: '<div><a class="btn btn-xs btn-danger feng-btn-delete" ng-click="grid.appScope.deleteSpend(row)" data="{{grid.getCellValue(row,col)}}">删除</a></div>'
+                    //ng-click时间触发的时候传入row
+             }],
+            paginationPageSizes :[5, 10, 20],
+            paginationPageSize: 10
+        };
+
+        $scope.updateSpend = function(row) {
+            //包装数据传递到后台
+            var obj = {
+                id: row.entity.id,
+                purchaser: row.entity.purchaser,
+                typeId: row.entity.Sort_id, //类别id
+                typeName: row.entity.Sort_name, //类别名
+                price: row.entity.Price,
+                purchaserDate:$filter('date')(row.entity.purchaserDate, 'yyyy-MM-dd'),
+                //字符串转化为日期，日期转化为年月日格式
+                purchaserPlace: row.entity.purchaserPlace,
+                currentNum: row.entity.Current_num,
+                brief: row.entity.Brief
+                    //缺少imageName,buyDate
+            };
+            // console.log(obj);
+            $http.put('/seeAllSpend/' + obj.id, obj).success(function(data, status) {
+                if (status === 200) {
+                    alert(data.success);
+                }
+            });
+        };
+
+        $scope.deleteSpend = function(row) {
+            $http.delete('/seeAllSpend/' + row.entity.id).success(function(data, status) {
+                if (status === 200) {
+                    $scope.getPagedDataAsync();
+                    alert(data.success);
+                }
+            });
+        };
+    }
+]);
+
+//添加管理员
+adminApp.controller('addAdmin', ['$scope', '$http',
+    function($scope, $http) {
+        //检验通过
+        $scope.addAdmin = function(valid, event) {
+            if ($scope.psw !== $scope.pswAgain) {
+                alert("两次密码输入不一致");
+                return;
+            }
+            $scope.formData = {
+                username: $scope.uName,
+                password: $scope.psw
+            };
+            if (valid) {
+                //发送数据到后台
+                $http({
+                    method: 'POST',
+                    url: '/addAdmin',
+                    headers: {
+                        //表单的报头格式
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    data: $.param($scope.formData) //发送user数据到后台，这里用到了jQ
+                }).then(function successCallback(response) {
+                    if (response.status === 200) {
+                        for (var p in $scope.formData) {
+                            $scope.uName = $scope.psw = $scope.pswAgain = '';
+                        }
+                        alert(response.data.success);
+                    }
+                }, function errorCallback(response) {
+                    alert(response.data.success);
+                });
+            }
+        }
+    }
+]);
+
+//管理当前账号
+adminApp.controller('manageAccount', ['$scope', '$http',
+    function($scope, $http) {
+
+        //检验通过
+        $scope.manageAccount = function(valid, event) {
+            if ($scope.psw !== $scope.pswAgain) {
+                alert("新密码两次密码输入不一致");
+                return;
+            }
+
+            $scope.formData = {
+                pswOld: $scope.pswOld,
+                username: $scope.uName,
+                password: $scope.psw
+            };
+            if (valid) {
+                //发送数据到后台
+                $http({
+                    method: 'POST',
+                    url: '/manageAccount',
+                    headers: {
+                        //表单的报头格式
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    data: $.param($scope.formData) //发送user数据到后台，这里用到了jQ
+                }).then(function successCallback(response) {
+                    if (response.status === 200) {
+                        for (var p in $scope.formData) {
+                            $scope.uName = $scope.psw = $scope.pswAgain = $scope.pswOld = '';
+                        }
+                        alert(response.data.success);
+                    }
+                }, function errorCallback(response) {
+                    alert(response.data.success);
+                });
+            }
+        }
+    }
+]);
---
+++ sql/daily.sql
@@ -0,0 +1,141 @@
+﻿# Host: localhost  (Version 5.7.16)
+# Date: 2017-08-25 17:41:36
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "t_academy"
+#
+
+DROP TABLE IF EXISTS `t_academy`;
+CREATE TABLE `t_academy` (
+  `Academy_id` varchar(10) NOT NULL,
+  `Academy_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`Academy_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "t_academy"
+#
+
+
+#
+# Structure for table "t_admin"
+#
+
+DROP TABLE IF EXISTS `t_admin`;
+CREATE TABLE `t_admin` (
+  `Admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Admin_name` varchar(10) NOT NULL,
+  `Admin_password` varchar(32) NOT NULL,
+  PRIMARY KEY (`Admin_id`),
+  UNIQUE KEY `Admin_id_UNIQUE` (`Admin_id`),
+  UNIQUE KEY `Admin_name` (`Admin_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "t_admin"
+#
+
+INSERT INTO `t_admin` VALUES (8,'hwf','c03223e4c2e74fdac0e5f7469073da26'),(9,'zsy','eb25d21e8a4d7dc52947b61ab26a2694'),(10,'admin','21232f297a57a5a743894a0e4a801fc3'),(11,'test','098f6bcd4621d373cade4e832627b4f6'),(12,'test2','ad0234829205b9033196ba818f7a872b'),(13,'test3','8ad8757baa8564dc136c1e07507f4a98'),(14,'test5','e3d704f3542b44a621ebed70dc0efe13'),(15,'test6','b04083e53e242626595e2b8ea327e525'),(16,'oo','e47ca7a09cf6781e29634502345930a7'),(17,'asd','7815696ecbf1c96e6894b779456d330e'),(18,'1','c4ca4238a0b923820dcc509a6f75849b');
+
+#
+# Structure for table "t_book_student"
+#
+
+DROP TABLE IF EXISTS `t_book_student`;
+CREATE TABLE `t_book_student` (
+  `Id` varchar(35) NOT NULL,
+  `Book_num` varchar(15) NOT NULL,
+  `Student_num` varchar(15) NOT NULL,
+  `borrow_date` date NOT NULL,
+  `return_date` date NOT NULL,
+  `Money` decimal(5,2) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "t_book_student"
+#
+
+
+#
+# Structure for table "t_class"
+#
+
+DROP TABLE IF EXISTS `t_class`;
+CREATE TABLE `t_class` (
+  `Class_id` varchar(10) NOT NULL,
+  `Class_name` varchar(30) NOT NULL,
+  `Academy_id` varchar(10) NOT NULL,
+  PRIMARY KEY (`Class_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "t_class"
+#
+
+
+#
+# Structure for table "t_spend"
+#
+
+DROP TABLE IF EXISTS `t_spend`;
+CREATE TABLE `t_spend` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `purchaser` varchar(100) NOT NULL DEFAULT '' COMMENT '购买者',
+  `Sort_id` int(11) NOT NULL,
+  `Price` decimal(5,2) DEFAULT NULL COMMENT '价格',
+  `purchaserPlace` varchar(20) DEFAULT NULL COMMENT '地点',
+  `purchaserDate` date DEFAULT '0000-00-00' COMMENT '时间',
+  `Current_num` int(11) NOT NULL DEFAULT '0' COMMENT '数量',
+  `Brief` text COMMENT '备注',
+  `imageName` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "t_spend"
+#
+
+
+#
+# Structure for table "t_student"
+#
+
+DROP TABLE IF EXISTS `t_student`;
+CREATE TABLE `t_student` (
+  `Student_num` varchar(15) NOT NULL,
+  `Student_name` varchar(10) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Academy_id` varchar(10) NOT NULL,
+  `Class_id` varchar(10) NOT NULL,
+  `Sex` varchar(2) DEFAULT NULL,
+  `Telephone` varchar(15) DEFAULT NULL,
+  `Email` varchar(20) DEFAULT NULL,
+  `Lended_num` int(11) NOT NULL DEFAULT '0',
+  `Create_date` date NOT NULL,
+  PRIMARY KEY (`Student_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "t_student"
+#
+
+
+#
+# Structure for table "t_type"
+#
+
+DROP TABLE IF EXISTS `t_type`;
+CREATE TABLE `t_type` (
+  `Sort_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Sort_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`Sort_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "t_type"
+#
+
+INSERT INTO `t_type` VALUES (1,'饮食'),(2,'服装'),(4,'水电'),(5,'交通'),(6,'通讯'),(7,'医疗'),(8,'租房'),(9,'旅游'),(10,'购物'),(13,'C#'),(14,'angular'),(15,'游戏'),(16,'test2');
